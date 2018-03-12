c02bd3a0ea0b18644408fb67448ea13a43bd58e9
stf279 <stf279@yahoo.com>
2018-03-12 15:06:38 -0500

    updated index and js files for pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Again, trying to do what I can in the time I gave myself
+
+'use strict';
+
+//Set variables for the characters
+
+//Set screen after choices
+
+//Determine outcome of battles to include defeat and continuing until either defeat or victory
+
+
