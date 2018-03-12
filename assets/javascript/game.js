1d7c73a46d61487f8102c4f7fbecb7a335ad7018
stf279 <stf279@yahoo.com>
2018-03-12 15:57:24 -0500

    updated index for images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,74 @@
 
 //Set variables for the characters
 
+let chars = [
+
+	luke = {
+
+		name: "Luke",
+
+		image: "<img src = 'assets/images/luke.jpg' id = 'luke'>",
+
+		defendImage: "<img src = 'assets/images/luke.jpg'>",
+
+		baseAttack: 10,
+
+		attack: 10,
+
+		health: 150,
+
+	},
+
+	vader = {
+
+		name: "Darth Vader",
+
+		image: "<img src = 'assets/images/vader.jpg' id = 'vader'>",
+
+		defendImage: "<img src = 'assets/images/vader.jpg'>",
+
+		baseAttack: 20,
+
+		attack: 20,
+
+        health: 180,
+        
+	},
+
+	maul = {
+
+		name: "Darth Maul",
+
+		image: "<img src = 'assets/images/maul.jpeg' id = 'maul'>",
+
+		defendImage: "<img src = 'assets/images/maul.jpeg'>",
+
+		baseAttack: 15,
+
+		attack: 15,
+
+        health: 120,
+        
+	},
+
+	yoda = {
+
+		name: "Yoda",
+
+		image: "<img src = 'assets/images/yoda.jpg' id = 'yoda'>",
+
+		defendImage: "<img src = 'assets/images/yoda.jpg'>",
+
+		baseAttack: 10, 
+
+		attack: 10,
+
+		health: 180,
+
+	}
+
+];
+
 //Set screen after choices
 
 //Determine outcome of battles to include defeat and continuing until either defeat or victory
